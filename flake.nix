fc406a2b0e669ca9e668a050029c53c20efc451f
Sergio del Rio Mayoral <sdelrio@users.noreply.github.com>
2024-05-20 11:25:57 +0200

    home-manager: visual studio code config and pluins, update flake, fix tabs to spaces

--- flake.nix
+++ flake.nix
@@ -21,87 +21,87 @@
   outputs = { self, nixpkgs, nixpkgs-unstable, nix-darwin, home-manager }@inputs:
     let
       mkHost = hostName: system:
-          nixpkgs.lib.nixosSystem {
-	    pkgs = import nixpkgs {
-              inherit system;
-	      config = { allowUnfree = true; };
-	    };
+        nixpkgs.lib.nixosSystem {
+	        pkgs = import nixpkgs {
+          inherit system;
+	        config = { allowUnfree = true; };
+	      };
 
-	    specialArgs = {
-              # By default, the system will only use packages from the
-              # stable channel.  You can selectively install packages
-              # from the unstable channel.  You can also add more
-              # channels to pin package version.
-              pkgs-unstable = import nixpkgs-unstable {
-                inherit system;
-                # settings to nixpkgs-unstable goes to here
-              };
+	      specialArgs = {
+          # By default, the system will only use packages from the
+          # stable channel.  You can selectively install packages
+          # from the unstable channel.  You can also add more
+          # channels to pin package version.
+          pkgs-unstable = import nixpkgs-unstable {
+            inherit system;
+            # settings to nixpkgs-unstable goes to here
+          };
 
-              # make all inputs availabe in other nix files
-              inherit inputs;
-	    };
+          # make all inputs availabe in other nix files
+          inherit inputs;
+	      };
 	    
-            modules = [
-              # Module 0: zfs-root
-              ./modules
+        modules = [
+          # Module 0: zfs-root
+          ./modules
 
-              # Module 1: host-specific config, if exist
-              #(if (builtins.pathExists ./hosts/${hostName}/configuration.nix) then
-              #  ./hosts/${hostName}/configuration.nix 
-              #else
-              #  { })
+          # Module 1: host-specific config, if exist
+          #(if (builtins.pathExists ./hosts/${hostName}/configuration.nix) then
+          #  ./hosts/${hostName}/configuration.nix 
+          #else
+          #  { })
 
-              # Module 2: entry point
+          # Module 2: entry point
 
-              # Module 4: config shared by all hosts
-              ./configuration.nix
+          # Module 4: config shared by all hosts
+          ./configuration.nix
 
-              # configuration input
-              ./hosts/${hostName}
+          # configuration input
+          ./hosts/${hostName}
 
-              # Module 3: home-manager
-              #home-manager.nixosModules.home-manager
-              #{
-              #  home-manager.useGlobalPkgs = true;
-              #  home-manager.useUserPackages = true;
-              #  #home-manager.users.sdelrio = import ./home.nix;
-              #}
-            ];
+          # Module 3: home-manager
+          #home-manager.nixosModules.home-manager
+          #{
+          #  home-manager.useGlobalPkgs = true;
+          #  home-manager.useUserPackages = true;
+          #  #home-manager.users.sdelrio = import ./home.nix;
+          #}
+        ];
           };
 
       mkMac = hostName: system: myUser:
-          nix-darwin.lib.darwinSystem {
-	    pkgs = import nixpkgs {
-        inherit system; # Makes system = system (x86_64-darwin or aarch64-darwin)
-	      config = { allowUnfree = true; };
-	    };
+        nix-darwin.lib.darwinSystem {
+	        pkgs = import nixpkgs {
+            inherit system; # Makes system = system (x86_64-darwin or aarch64-darwin)
+	          config = { allowUnfree = true; };
+	        };
 
-            modules = [
+          modules = [
 
-	            ./modules/darwin
+	          ./modules/darwin
 
-              # configuration input
-              ./hosts/${hostName}
+            # configuration input
+            ./hosts/${hostName}
 
-              # Module home-manager
+            # Module home-manager
 
-              # https://github.com/nix-community/home-manager/issues/4026#issuecomment-1565974702
-              ({ users.users.${myUser}.home = "/Users/${myUser}"; })
+            # https://github.com/nix-community/home-manager/issues/4026#issuecomment-1565974702
+            ({ users.users.${myUser}.home = "/Users/${myUser}"; })
 
-              home-manager.darwinModules.home-manager
-              {
-                home-manager = {
-                  useGlobalPkgs = true;
-                  useUserPackages = true;
-		              users.${myUser}.imports = [
-		                ./modules/home-manager
-		                ./users/${myUser}/hm.nix
-                    #./hosts/${hostName}/hm.nix
-		              ];
-                };
-              }
-            ];
-          };
+            home-manager.darwinModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+		            users.${myUser}.imports = [
+		              ./modules/home-manager
+		              ./users/${myUser}/hm.nix
+                  ./hosts/${hostName}/hm.nix
+		            ];
+              };
+            }
+          ];
+        };
     in {
       nixosConfigurations = {
         exampleHost = mkHost "exampleHost" "x86_64-linux";
